23c5e657dee47a85cfcf45bf5043c7ce1ad6ea8d
Gujarats <gujarat.santana@gmail.com>
2018-04-05 17:06:40 +0700

    using data for generate json policy

--- iam-artifact-role-get-access.tf
+++ iam-artifact-role-get-access.tf
@@ -11,34 +11,21 @@ data "aws_iam_policy_document" "developers_get_credentials_assume_role_policy" {
 
 resource "aws_iam_role" "developers_get_credentials" {
   description = "role for another acoount to get iam user which has read access to s3"
-  name = "${var.role_developers_get_credentials}"
+  name ="${random_id.role_name_get_credentials.hex}"
   force_detach_policies = true
   assume_role_policy = "${data.aws_iam_policy_document.developers_get_credentials_assume_role_policy.json}"
 }
 
-resource "aws_iam_policy" "get_credentials_parameter_store" {
-    name        = "${var.policy_developers_get_credentials}"
-    description = "For getting the acces & secret keys from parameter store"
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
-                "ssm:GetParameters",
-                "ssm:GetParameter"
-            ],
-            "Resource": "${var.resource_ssm_credentials}"
-        }
-    ]
-}
-EOF
+data "aws_iam_policy_document" "get_credentials_parameter_store_policy" {
+  statement {
+    actions = ["ssm:GetParameter"]
+    effect = "Allow" 
+    resources = ["*"]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "developers_get_credentials_attachment" {
-    role       = "${aws_iam_role.developers_get_credentials.name}"
-    policy_arn = "${aws_iam_policy.get_credentials_parameter_store.arn}"
+resource "aws_iam_role_policy" "get_credentials_parameter_store" {
+  name = "${var.product_domain}GetCredentialsParameterStore"
+  role = "${aws_iam_role.developers_get_credentials.id}"
+  policy = "${data.aws_iam_policy_document.get_credentials_parameter_store_policy.json}"
 }
-
