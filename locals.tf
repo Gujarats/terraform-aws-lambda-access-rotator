44a08e873f73934046070923a948801444ccdb44
Gujarats <gujarat.santana@gmail.com>
2018-04-05 20:23:39 +0700

    update locals.tf random var

--- locals.tf
+++ locals.tf
@@ -1,19 +1,19 @@
 resource "random_id" "lambda_role_name" {
-  prefix      = "${var.product_domain}-lambda-rotator"
+  prefix      = "${var.product_domain}-lamba-role-"
   byte_length = 8
 }
 
 resource "random_id" "lambda_function_name" {
-    prefix = "${var.product_domain}-rotator-keys"
+    prefix = "${var.product_domain}-rotator-keys-"
     byte_length = 8
 }
 
 resource "random_id" "iam_user_s3_reader" {
-    prefix = "${var.product_domain}-artifact-reader"
+    prefix = "${var.product_domain}-artifact-reader-"
     byte_length = 8
 }
 
 resource "random_id" "role_name_get_credentials" {
-    prefix = "${var.product_domain}-get-credentials"
+    prefix = "${var.product_domain}-get-credentials-"
     byte_length = 8
 }
